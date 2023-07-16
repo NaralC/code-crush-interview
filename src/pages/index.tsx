97c656502d64c9394319e1a162c1b00a0343d838
Naral <77269201+NaralC@users.noreply.github.com>
2023-07-16 21:18:05 +0100

    Create Next App + UI libraries set up

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-24">
+      <div>Code Crush</div>
+      <div>Find a peer for coding interviews!</div>
+      <div>
+        Start today with our free online interview prep tool. No signup
+        required. Invite a peer, pick a room, and start coding.
+      </div>
+    </main>
+  );
+}
