6b1b401eacb82fd5a56cda32329416f8748a7750
Naral <77269201+NaralC@users.noreply.github.com>
2024-01-29 20:10:28 +0000

    E2E test for creating a new room

---
+++ cypress/e2e/create-room.cy.js
@@ -0,0 +1,50 @@
+/// <reference types="Cypress" />
+
+describe("Creating a new room", () => {
+    it('fails when an interview type is not selected', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.contains('Create a Room').click()
+        cy.get('input[name="roomName"]').type('E2E Test Room')
+        cy.get('input[name="userName"]').type('Naral')
+        cy.contains("Proceed").click()
+        cy.contains("You need to select an interview type.").should('be.visible')
+    })
+
+    it('fails when the room name is empty', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.contains('Create a Room').click()
+        cy.get('input[name="userName"]').type('Naral')
+        cy.get('[value="ds_algo"]').click({ multiple: true, force: true })
+        cy.contains("Proceed").click()
+        cy.contains("Room name cannot be empty").should('be.visible')
+    })
+
+    it('fails when the username is empty', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.contains('Create a Room').click()
+        cy.get('input[name="roomName"]').type('E2E Test Room')
+        cy.get('[value="ds_algo"]').click({ multiple: true, force: true })
+        cy.contains("Proceed").click()
+        cy.contains("Name cannot be empty").should('be.visible')
+    })
+
+    it('succeeds and navigate to it when necessary fields are filled', () => {
+        cy.visit('http://localhost:3000/')
+
+        cy.contains('Create a Room').click()
+        cy.get('input[name="roomName"]').type('E2E Test Room')
+        cy.get('input[name="userName"]').type('Naral')
+        cy.get('[value="ds_algo"]').click({ multiple: true, force: true })
+
+        // Create room
+        cy.intercept('POST', '/api/db').as('createRoom')
+        cy.contains("Proceed").click()
+        cy.wait('@createRoom', { timeout: 10000 }) // shouldn't take more than 10 seconds
+        cy.url().should('include', '/code/ds-algo/')
+    })
+
+    // TODO: creating a front-end room
+})
