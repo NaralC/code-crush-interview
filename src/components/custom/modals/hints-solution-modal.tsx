fa597cb3118c753ddc907fdc5649a9b731985b6d
Naral <77269201+NaralC@users.noreply.github.com>
2024-03-12 01:00:46 +0000

    Hints and Solutions are now multiple-lines

--- src/components/custom/modals/hints-solution-modal.tsx
+++ src/components/custom/modals/hints-solution-modal.tsx
@@ -7,14 +7,25 @@ const HintsSolutionModal: FC = () => {
     hintsSolutionModal: { isOpen, setClose, type, body },
   } = useModalStore();
   
+  const safeParseJSON = (jsonString: string) => {
+    try {
+      return JSON.parse(jsonString);
+    } catch (e) {
+      console.error('Error parsing JSON:', e);
+      return null;
+    }
+  };
+
+  const parsedBody = safeParseJSON(body);
+
   return (
     <Modal
       title={type === "hints" ? "Hint(s)" : "Solution(s)"}
       isOpen={isOpen}
       setClose={setClose}
     >
-      <div className="p-2 overflow-x-auto text-white rounded-md bg-slate-950 selection:bg-cyan-700">
-        {body}
+      <div className="p-2 overflow-x-auto text-white rounded-md bg-slate-950 selection:bg-cyan-700 ">
+        <pre className="overflow-auto break-words whitespace-pre-wrap">{parsedBody}</pre>
       </div>
     </Modal>
   );
--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -192,10 +192,10 @@ const QuestionsPage: NextPage = () => {
           <Label htmlFor="hint" className="text-2xl">
             Hint(s)
           </Label>
-          <Input
+          <textarea
             id="hint"
             placeholder="Hint(s)..."
-            className="text-black"
+            className="flex w-full h-48 px-3 py-2 text-sm text-black transition bg-white border rounded-md border-slate-200 ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-800"
             value={textInputs.hints!}
             onChange={(e) =>
               setTextInputs((prev) => ({ ...prev, hints: e.target.value }))
@@ -207,10 +207,10 @@ const QuestionsPage: NextPage = () => {
           <Label htmlFor="solution" className="text-2xl">
             Solution(s)
           </Label>
-          <Input
+          <textarea
             id="solution"
             placeholder="Solutions..."
-            className="text-black"
+            className="flex w-full h-48 px-3 py-2 text-sm text-black transition bg-white border rounded-md border-slate-200 ring-offset-white file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-400 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:border-slate-800 dark:bg-slate-950 dark:ring-offset-slate-950 dark:placeholder:text-slate-400 dark:focus-visible:ring-slate-800"
             value={textInputs.solution!}
             onChange={(e) =>
               setTextInputs((prev) => ({ ...prev, solution: e.target.value }))
